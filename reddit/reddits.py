ce7dc3853df02b0daee6c0f05ca9799e8c8438ba
itgsod-Daniel-Roos <daniel.roos@elev.it-gymnasiet.se>
2015-04-22 14:43:02 +0200

    Experimenting with comments
    Added class “Comments”

--- reddit/reddits.py
+++ reddit/reddits.py
@@ -7,6 +7,8 @@ url_hot = "https://oauth.reddit.com/r/{subreddit}/hot"
 url_wiki_contrib = "https://oauth.reddit.com/r/{subreddit}/about/wikicontributors"
 url_mod_list = "https://oauth.reddit.com/r/{subreddit}/about/moderators"
 
+comment_url_hot = "https://oauth.reddit.com/r/{subreddit}/comments/{article}"
+
 class Subreddit(object):
 
     def __init__(self, subreddit):
@@ -21,3 +23,13 @@ class Subreddit(object):
     def mod_list (self):
 
         return reddit.client.request(url_mod_list.format(subreddit=self.subreddit))
+
+class Comments(object):
+
+    def __init__(self, subreddit, article):
+        self.subreddit = subreddit
+        self.article = article
+
+    def hot(self):
+        return reddit.client.request(comment_url_hot.format(subreddit=self.subreddit, article=self.article))
+
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from reddit  import client
 from reddit.user import User
-from reddit.reddits import Subreddit
+from reddit.reddits import *
 daniel = client.login("playing_blindfolded")
 
 #print daniel.submit('danielsbot', 'self', 'test', 'test_text')
@@ -9,12 +9,19 @@ daniel = client.login("playing_blindfolded")
 
 #print(daniel.karma_per_reddit())
 
-usage_info = Subreddit('danielsbot').hot()['data']['children']
+#usage_info = Subreddit('danielsbot').hot()['data']['children']
 
-for i in range (0, 24):
-    if usage_info[i]['data']['title'] == 'test':
-        daniel.comment(usage_info[i]['data']['name'], 'selftext = ' + usage_info[i]['data']['selftext'])
+#for i in range (0, 24):
+#    if usage_info[i]['data']['title'] == 'test':
+#        daniel.comment(usage_info[i]['data']['name'], 'selftext = ' + usage_info[i]['data']['selftext'])
 
 #print (Subreddit('danielsbot').hot()['data']['children'][0]['data']['selftext'])
 
-#python.hot()
+usage_info = (Comments('danielsbot', '33gez8').hot()[1]['data']['children'])
+#for i in range (0, len(usage_info)):
+#    if usage_info[i]['data']['body'] == 'testcomment':
+#        daniel.comment(usage_info[i]['data']['name'], 'You said: ' + usage_info[i]['data']['body'])
+
+print (usage_info[0]['data']['replies']['data']['children'][0]['data']['replies']['data']['children'][0]['data']['name'])
+
+
