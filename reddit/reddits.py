4437cfc3c587672546e84a2ebed99ead1f05e764
itgsod-Daniel-Roos <daniel.roos@elev.it-gymnasiet.se>
2015-05-06 10:45:10 +0200

    Added a bunch of stuff.
    Yup.

--- reddit/reddits.py
+++ reddit/reddits.py
@@ -7,7 +7,7 @@ url_hot = "https://oauth.reddit.com/r/{subreddit}/hot"
 url_wiki_contrib = "https://oauth.reddit.com/r/{subreddit}/about/wikicontributors"
 url_mod_list = "https://oauth.reddit.com/r/{subreddit}/about/moderators"
 
-comment_url_hot = "https://oauth.reddit.com/r/{subreddit}/comments/{article}"
+comment_url = "https://oauth.reddit.com/r/{subreddit}/comments/{article}"
 
 class Subreddit(object):
 
@@ -31,5 +31,12 @@ class Comments(object):
         self.article = article
 
     def hot(self):
-        return reddit.client.request(comment_url_hot.format(subreddit=self.subreddit, article=self.article))
+        return reddit.client.request(comment_url.format(subreddit=self.subreddit, article=self.article))
 
+    def search_hot(self, user, subreddit, keyword, fullname, reply):
+        info_dic = (Comments(subreddit, fullname).hot()[1]['data']['children'])
+        for i in range (0, len(info_dic)):
+            if info_dic[i]['data']['body'] == keyword:
+                user.comment(info_dic[i]['data']['name'], 'You said: ' + info_dic[i]['data']['body'])
+            info_dic += [i]['data']['replies']['data']['children']
+            Comments.search_hot(self, user, subreddit, keyword, fullname, reply)
--- test.py
+++ test.py
@@ -17,11 +17,22 @@ daniel = client.login("playing_blindfolded")
 
 #print (Subreddit('danielsbot').hot()['data']['children'][0]['data']['selftext'])
 
-usage_info = (Comments('danielsbot', '33gez8').hot()[1]['data']['children'])
+#usage_info = (Comments('danielsbot', '33gez8').hot()[1]['data']['children'])
 #for i in range (0, len(usage_info)):
 #    if usage_info[i]['data']['body'] == 'testcomment':
 #        daniel.comment(usage_info[i]['data']['name'], 'You said: ' + usage_info[i]['data']['body'])
 
-print (usage_info[0]['data']['replies']['data']['children'][0]['data']['replies']['data']['children'][0]['data']['name'])
+#print (usage_info[0]['data']['replies']['data']['children'][0]['data']['replies']['data']['children'][0]['data']['name'])
 
+def search_hot(self, subreddit, keyword, fullname, reply):
+        info_dic = (Comments(subreddit, fullname).hot()[1]['data']['children'])
 
+        if len(info_dic['data']['replies']) >= 1:
+            for i in range (0, len(info_dic)):
+                if info_dic[i]['data']['body'] == keyword:
+                    daniel.comment(info_dic[i]['data']['name'], reply)
+                    i += 1
+                info_dic = info_dic[i]['data']['replies']['data']['children']
+                search_hot(self, subreddit, keyword, fullname, reply)
+
+search_hot(1, 'danielsbot', 'testcomment', '33gez8', 'this is my reply')
